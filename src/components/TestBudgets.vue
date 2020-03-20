bb34852e9736189b35323b3e5abf1e2505cf5763
dragos <dragos@1stweb.dk>
2020-03-20 21:28:14 +0200

    initial commit

---
+++ src/components/TestBudgets.vue
@@ -0,0 +1,36 @@
+<template>
+
+  <div>
+
+    <button @click="onClick">click me</button>
+    xxx
+
+  </div>
+
+</template>
+
+<script>
+
+import Vue from 'vue';
+
+export default {
+
+  mounted() {
+    console.log(this)
+  },
+
+  methods: {
+    onClick(budget) {
+      console.log(new Date())
+      console.log(new Date())
+    }
+  }
+
+}
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/TestContainer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <slot :visible="visible"></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component
+export default class extends Vue {
+  get visible() {
+    return 'xxxxx visible xxxx'
+  }
+
+  afterUpdate() {
+    console.log('afterUpdate')
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
